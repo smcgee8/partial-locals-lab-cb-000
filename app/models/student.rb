8467d650eef0ce42db81b490fc787b395a3755b8
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-10 15:36:12 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,8 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(search_term)
+    Student.where('name LIKE ?', "%#{search_term}%")
+  end
 end
